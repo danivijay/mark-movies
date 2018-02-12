06beec84467f300aba69e14fe6dfc54e31c37300
Dani Vijay <danimvijay@gmail.com>
2018-02-12 17:23:46 +0530

    client init setup

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-toolbar
+      app
+    >
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer fixed="false" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Mark Movies'
+    }
+  },
+  name: 'App'
+}
+</script>
