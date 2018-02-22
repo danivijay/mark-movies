03f9ace51071bb6495b3a82be6fe4f314d2f30da
Dani Vijay <danimvijay@gmail.com>
2018-02-22 19:15:20 +0530

    add body parser, morgan, cors

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,6 +1,14 @@
 import express from 'express'
 const app = express()
 
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
 import movieRoutes from './api/routes/movies'
 
 app.use('/movies', movieRoutes)
