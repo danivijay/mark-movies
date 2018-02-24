47f0d7edb3d1f4ede3ba80fab2dc09b3454b58af
Dani Vijay <danimvijay@gmail.com>
2018-02-24 18:24:57 +0530

    server - error handler

--- server/lib/app.js
+++ server/lib/app.js
@@ -13,4 +13,19 @@ import movieRoutes from './api/routes/movies'
 
 app.use('/movies', movieRoutes)
 
+app.use((req, res, next) => {
+  const err = new Error('not found')
+  err.status(404)
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    err: {
+      msg: err.message
+    }
+  })
+})
+
 export default app
