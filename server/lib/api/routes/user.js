5eb87d9ab1eaa101aac659d7e91bf67ef8b8554e
Dani Vijay <danimvijay@gmail.com>
2018-03-01 21:34:32 +0530

    server - hash password

--- server/lib/api/routes/user.js
+++ server/lib/api/routes/user.js
@@ -1,28 +1,42 @@
 import express from 'express'
 const router = express.Router()
 
+import bcrypt from 'bcrypt'
+const saltRounds = 10;
+
 import models from '../../models'
 
 router.post('/signup', (req, res, next) => {
-  if (!req.body.email && !req.body.password) {
-    console.log('email or password missing')
+  if (!req.body.email) {
+    console.log('email missing')
     return false
   }
-  const userinfo = {
-    email: req.body.email,
-    password: req.body.password
-  }
-  models.User.create(userinfo)
-    .then(user => {
-      res.status(200).json({
-        msg : 'signup success',
-        user : user.toJSON()
+  if (!req.body.password) {
+    console.log('password missing')
+    return false
+  } else {
+      bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        if (err) {
+          console.log('hashing failed')
+          return false
+        }
+        const userinfo = {
+          email: req.body.email,
+          password: hash
+        }
+        models.User.create(userinfo)
+          .then(user => {
+            res.status(200).json({
+              msg : 'signup success',
+              user : user.toJSON()
+            })
+            res.send()
+          })
+          .catch(err => {
+            console.log(err)
+          })
       })
-      res.send()
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  }
 })
 
 router.post('/signin', (req, res, next) => {
