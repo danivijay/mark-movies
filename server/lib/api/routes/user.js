7729b45083ede93b5922e967c375b890c2ecfd13
Dani Vijay <danimvijay@gmail.com>
2018-03-01 20:50:38 +0530

    server signup basic

--- server/lib/api/routes/user.js
+++ server/lib/api/routes/user.js
@@ -1,13 +1,32 @@
 import express from 'express'
 const router = express.Router()
 
+import models from '../../models'
+
 router.post('/signup', (req, res, next) => {
-  res.status(200).json({
-    msg : 'reached signup'
-  })
+  if (!req.body.email && !req.body.password) {
+    console.log('email or password missing')
+    return false
+  }
+  const userinfo = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  models.User.create(userinfo)
+    .then(user => {
+      res.status(200).json({
+        msg : 'signup success',
+        user : user.toJSON()
+      })
+      res.send()
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
 router.post('/signin', (req, res, next) => {
+  console.log(req.body)
   res.status(200).json({
     msg : 'reached signin'
   })
